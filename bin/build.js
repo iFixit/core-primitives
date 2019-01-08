1d7a01bfa875537b1cc5a95cf5236b27180b2be2
Cole Bemis <colebemis@gmail.com>
2019-01-07 22:26:07 -0800

    Create build script

---
+++ bin/build.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+const kebabCase = require('lodash.kebabcase')
+const primitives = require('..')
+
+fs.writeFileSync(
+  path.resolve(__dirname, '../variables.less'),
+  `/* GENERATED FILE. DO NOT EDIT. */
+${toLess(primitives)}`,
+)
+
+function toLess(obj) {
+  return Object.entries(flatten(obj))
+    .map(([key, value]) => `@${key}: ${value};`)
+    .join('\n')
+}
+
+function flatten(obj) {
+  return Object.assign(
+    {},
+    ...Object.entries(obj).map(([key, value]) => {
+      if (typeof value === 'object') {
+        return Object.entries(flatten(value)).reduce(
+          (acc, entry) => ({
+            ...acc,
+            [`${kebabCase(key)}-${kebabCase(entry[0])}`]: entry[1],
+          }),
+          {},
+        )
+      } else {
+        return { [kebabCase(key)]: value }
+      }
+    }),
+  )
+}
