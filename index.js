6bd9f4e218edb372f9b9132eab11fddda536fb90
Cole Bemis <colebemis@gmail.com>
2019-01-11 16:35:37 -0800

    Add more colors

--- index.js
+++ index.js
@@ -4,7 +4,57 @@ const color = {
   yellow: '#F9C03E',
   red: '#F34E46',
   white: '#FFFFFF',
-  black: '#000000',
+  black: '#11161A',
+
+  // Blues
+  blueLight4: '#EDF6FF',
+  blueLight3: '#BDDCFF',
+  blueLight2: '#77B5FF',
+  blueLight1: '#3B95FF',
+  blueDark1: '#085FD9',
+  blueDark2: '#004AB3',
+  blueDark3: '#00368C',
+  blueDark4: '#002466',
+
+  // Greens
+  greenLight4: '#E8FAEE',
+  greenLight3: '#B7ECC8',
+  greenLight2: '#89DCA5',
+  greenLight1: '#5DC984',
+  greenDark1: '#269C55',
+  greenDark2: '#1B8246',
+  greenDark3: '#126637',
+  greenDark4: '#0A4927',
+
+  // Yellows
+  yellowLight4: '#FFF8E9',
+  yellowLight3: '#FFE9B9',
+  yellowLight2: '#FFDB8C',
+  yellowLight1: '#FFCD63',
+  yellowDark1: '#D99B2E',
+  yellowDark2: '#B37820',
+  yellowDark3: '#8C5715',
+  yellowDark4: '#66390C',
+
+  // Reds
+  redLight4: '#FFEDED',
+  redLight3: '#FFB8B3',
+  redLight2: '#FF8B85',
+  redLight1: '#FF6860',
+  redDark1: '#D93531',
+  redDark2: '#B3201F',
+  redDark3: '#8C1113',
+  redDark4: '#66070B',
+
+  // Grays
+  gray1: '#F7F9FA',
+  gray2: '#E3E8ED',
+  gray3: '#D2DADF',
+  gray4: '#AFBBC4',
+  gray5: '#828D96',
+  gray6: '#525D66',
+  gray7: '#39444D',
+  gray8: '#242C33',
 }
 
 module.exports = { color }
