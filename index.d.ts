c7785668dcc84376e1a3e10a29b2e6fafdf3bfbc
Ian Rohde <ian@ianrohde.com>
2021-09-27 14:34:37 -0700

    extend ColorValueBlue interface

--- index.d.ts
+++ index.d.ts
@@ -24,17 +24,7 @@ interface ColorValue {
   "900": string;
 }
 
-interface ColorValueBlue {
-  "50": string;
-  "100": string;
-  "200": string;
-  "300": string;
-  "400": string;
-  "500": string;
-  "600": string;
-  "700": string;
-  "800": string;
-  "900": string;
+interface ColorValueBlue extends ColorValue {
   "ifixit": string;
 }
 
