b574fc816cab66bd1b079b81456f5517277ef5dd
Ian Rohde <ian@ianrohde.com>
2021-11-10 11:34:08 -0800

    add transition primitives, add colors, refactor ts
    - index.json: replace black
    - index.json: add red.dozuki
    - index.d.ts: types when applicable
    - index.d.ts: export named primitives
    - index.d.ts: formatting

--- index.d.ts
+++ index.d.ts
@@ -1,126 +1,139 @@
 interface Primitives {
-  color: Color;
-  space: Space;
-  fontFamily: FontFamily;
-  fontSettings: FontSettings;
-  fontSize: FontSize;
-  fontWeight: FontWeight;
-  lineHeight: LineHeight;
-  breakpoint: Breakpoint;
-  shadow: Shadow;
-  borderRadius: BorderRadius;
+  color: Color
+  space: Space
+  fontFamily: FontFamily
+  fontSettings: FontSettings
+  fontSize: FontSize
+  fontWeight: FontWeight
+  lineHeight: LineHeight
+  breakpoint: Breakpoint
+  shadow: Shadow
+  borderRadius: BorderRadius
+  transition: Transition
 }
 
-interface ColorValue {
-  "50": string;
-  "100": string;
-  "200": string;
-  "300": string;
-  "400": string;
-  "500": string;
-  "600": string;
-  "700": string;
-  "800": string;
-  "900": string;
+type ColorValue = {
+  "50": string
+  "100": string
+  "200": string
+  "300": string
+  "400": string
+  "500": string
+  "600": string
+  "700": string
+  "800": string
+  "900": string
 }
 
 interface ColorValueBlue extends ColorValue {
-  "ifixit": string;
+  ifixit: string
+}
+
+interface ColorValueRed extends ColorValue {
+  dozuki: string
 }
 
 interface Color {
-  black: string;
-  white: string;
-  rose: ColorValue;
-  pink: ColorValue;
-  fuchsia: ColorValue;
-  purple: ColorValue;
-  violet: ColorValue;
-  indigo: ColorValue;
-  blue: ColorValueBlue;
-  lightBlue: ColorValue;
-  cyan: ColorValue;
-  teal: ColorValue;
-  emerald: ColorValue;
-  green: ColorValue;
-  lime: ColorValue;
-  yellow: ColorValue;
-  amber: ColorValue;
-  orange: ColorValue;
-  red: ColorValue;
-  gray: ColorValue;
+  black: string
+  white: string
+  rose: ColorValue
+  pink: ColorValue
+  fuchsia: ColorValue
+  purple: ColorValue
+  violet: ColorValue
+  indigo: ColorValue
+  blue: ColorValueBlue
+  lightBlue: ColorValue
+  cyan: ColorValue
+  teal: ColorValue
+  emerald: ColorValue
+  green: ColorValue
+  lime: ColorValue
+  yellow: ColorValue
+  amber: ColorValue
+  orange: ColorValue
+  red: ColorValueRed
+  gray: ColorValue
 }
 
-type Space = string[];
+type Space = string[]
 
-interface FontFamily {
-  arialBlack: string;
-  inter: string;
-  lato: string;
-  mono: string;
-  monoSystem: string;
-  sansSystem: string;
-  serifSystem: string;
+type FontFamily = {
+  arialBlack: string
+  inter: string
+  lato: string
+  mono: string
+  monoSystem: string
+  sansSystem: string
+  serifSystem: string
 }
 
-interface FontSettings {
-  inter: string;
+type FontSettings = {
+  inter: string
 }
 
 type FontSize = {
-  sm: string;
-  md: string;
-  lg: string;
-  xl: string;
-  "2xl": string;
-  "3xl": string;
-  "4xl": string;
-  "5xl": string;
-  "6xl": string;
-  "7xl": string;
-  "8xl": string;
-  "9xl": string;
+  sm: string
+  md: string
+  lg: string
+  xl: string
+  "2xl": string
+  "3xl": string
+  "4xl": string
+  "5xl": string
+  "6xl": string
+  "7xl": string
+  "8xl": string
+  "9xl": string
 }
 
-interface FontWeight {
-  normal: number;
-  semiBold: number;
-  bold: number;
+type FontWeight = {
+  normal: number
+  semiBold: number
+  bold: number
 }
 
-interface LineHeight {
-  none: number;
-  tight: number;
-  normal: number;
-  loose: number;
+type LineHeight = {
+  none: number
+  tight: number
+  normal: number
+  loose: number
 }
 
-interface Breakpoint {
-  sm: string;
-  md: string;
-  lg: string;
-  xl: string;
+type Breakpoint = {
+  sm: string
+  md: string
+  lg: string
+  xl: string
 }
 
-type Shadow = string[];
+type Shadow = string[]
 
-interface BorderRadius {
-  sm: string;
-  md: string;
-  lg: string;
-  xl: string;
-  pill: string;
+type BorderRadius = {
+  sm: string
+  md: string
+  lg: string
+  xl: string
+  pill: string
 }
 
-export declare const color: Color;
-export declare const space: Space;
-export declare const fontFamily: FontFamily;
-export declare const fontSettings: FontSettings;
-export declare const fontSize: FontSize;
-export declare const fontWeight: FontWeight;
-export declare const lineHeight: LineHeight;
-export declare const breakpoint: Breakpoint;
-export declare const shadow: Shadow;
-export declare const borderRadius: BorderRadius;
-declare const primitives: Primitives;
-export default primitives;
+type Transition = {
+  "100": string
+  "150": string
+  "200": string
+  "250": string
+}
+
+export declare const color: Color
+export declare const space: Space
+export declare const fontFamily: FontFamily
+export declare const fontSettings: FontSettings
+export declare const fontSize: FontSize
+export declare const fontWeight: FontWeight
+export declare const lineHeight: LineHeight
+export declare const breakpoint: Breakpoint
+export declare const shadow: Shadow
+export declare const borderRadius: BorderRadius
+export declare const transition: Transition
+export declare const primitives: Primitives
+export default primitives
